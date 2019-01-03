8cd3a8d1e0c1ab5edd92f90e7e287c3d963c7575
karnwatcharasupat <43736781+karnwatcharasupat@users.noreply.github.com>
2019-01-03 17:06:04 +0800

    Added DEBUG serial prints

--- NeopixelCtrl/NeopixelCtrlExample/NeopixelCtrl.cpp
+++ NeopixelCtrl/NeopixelCtrlExample/NeopixelCtrl.cpp
@@ -5,6 +5,8 @@
   Garage@EEE, School of Electrical and Electronic Engineering, Nanyang Technological University, Singapore.
 */
 
+#define NEOPIXEL_DEBUG true
+
 #include "NeopixelCtrl.h"
 
 NeopixelCtrl :: NeopixelCtrl (Adafruit_NeoPixel* pixelsPtr, int totalLength, int pixelsPin) {
@@ -15,9 +17,17 @@ NeopixelCtrl :: NeopixelCtrl (Adafruit_NeoPixel* pixelsPtr, int totalLength, int
   _isCountingDown = false;
   _isCountingUp = false;
   _isFrenzy = false;                 // initialize all actions to false
+
+#if NEOPIXEL_DEBUG
+  Serial.println("The NeopixelCtrl object is created.");
+#endif
 }
 
 void NeopixelCtrl :: setTopSegment(int firstPixel, int lastPixel) {
+#if NEOPIXEL_DEBUG
+  Serial.println("Setting the top segment...");
+#endif
+
   _topSegmentFirstIndex = firstPixel;
   _topSegmentLastIndex = lastPixel;
 
@@ -26,27 +36,45 @@ void NeopixelCtrl :: setTopSegment(int firstPixel, int lastPixel) {
   if (_topSegmentLength % 2 != 0) {
     _leftSegmentLength = _topSegmentLength / 2 + 1;
     _rightSegmentLength = _topSegmentLength - _leftSegmentLength;
+
+#if NEOPIXEL_DEBUG
+    Serial.println("Warning: The top segment has odd number of pixels!");
+#endif
   }
   else {
     _leftSegmentLength = _topSegmentLength / 2;
     _rightSegmentLength = _leftSegmentLength;
+#if NEOPIXEL_DEBUG
+    Serial.println("The top segment has even number of pixels!");
+#endif
   }                                               // in case of asymmetrical display
   // or if one pixel goes off the code can still sort of run with just minor index adjustment
 
   _rightSegmentFirstIndex = _topSegmentFirstIndex + _leftSegmentLength;
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Done setting the top segment.");
+#endif
 }
 
 void NeopixelCtrl :: setPlayerSegment(int playerCode, int firstPixel, int lastPixel) {
+#if NEOPIXEL_DEBUG
+  Serial.print("Setting Player "); Serial.print(playerCode); Serial.println("'s segment...");
+#endif
+
   _playerPixelIndices[playerCode - 1][0] = firstPixel;
-  Serial.println("Set first  pixel");
   _playerPixelIndices[playerCode - 1][1] = lastPixel;
-  Serial.println("Set last pixel");
   _playerPixelLengths[playerCode - 1] = lastPixel - firstPixel + 1;
-  Serial.println("Set length");
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Done.");
+#endif
 }
 
 uint32_t NeopixelCtrl :: getColour(char colour) {
-  Serial.println("Getting colour");
+#if NEOPIXEL_DEBUG
+  Serial.print("Getting colour...");
+#endif
   switch (colour) {
     case 'R':
       return _pixelsPtr->Color(255, 0, 0);
@@ -63,15 +91,22 @@ uint32_t NeopixelCtrl :: getColour(char colour) {
     default:
       return _pixelsPtr->Color(255, 255, 255);                      //return white if the colour input in undefined
   }
+#if NEOPIXEL_DEBUG
+  Serial.println("Done.");
+#endif
 }
 
 void NeopixelCtrl :: setPlayerSegmentColour(int playerCode, char colour) {
-  Serial.println("Setting Colour");
+#if NEOPIXEL_DEBUG
+  Serial.print("Setting colour...");
+#endif
   _playerColours[playerCode - 1] = getColour(colour);
 }
 
 void NeopixelCtrl :: countDown(int playerCode1, int playerCode2, int duration, unsigned long startTime) {
-
+#if NEOPIXEL_DEBUG
+  Serial.println("countDown() is called.");
+#endif
   if (_isCountingUp || _isFrenzy) {
     _isCountingUp = false;
     _isFrenzy = false;
@@ -88,34 +123,85 @@ void NeopixelCtrl :: countDown(int playerCode1, int playerCode2, int duration, u
   _countdownPlayerLeft = playerCode1;
   _countdownPlayerRight = playerCode2;
 
+#if NEOPIXEL_DEBUG
+  Serial.print("Left player is Player "); Serial.println(_countdownPlayerLeft);
+  Serial.print("Right player is Player "); Serial.println(_countdownPlayerRight);
+#endif
+
   _countdownStartTime = startTime;
   _countdownDuration = duration * 1000UL; // convert milliseconds to seconds, UL for typecasting to unsigned long
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Countdown start time is "); Serial.println(_countdownStartTime);
+  Serial.print("Counting down for "); Serial.print(_countdownDuration); Serial.print("ms.");
+#endif
 }
 
 void NeopixelCtrl :: updateCountDown(unsigned long currentTime) {
 
+#if NEOPIXEL_DEBUG
+  Serial.println("updateCountDown() is called.");
+#endif
+
   unsigned long timeElapsed = currentTime - _countdownStartTime;
+
+#if NEOPIXEL_DEBUG
+  Serial.print(timeElapsed);
+  Serial.println(" ms has elapsed.");
+#endif
 
   if (timeElapsed > _countdownDuration) {
     _isCountingDown = false;
+
+#if NEOPIXEL_DEBUG
+    Serial.println("Duration exceeded. Exiting the function.");
+#endif
+
     return;
   }
 
   int numPixelLeft = map(timeElapsed, 0, _countdownDuration, _leftSegmentLength, 0);
   int numPixelRight = map(timeElapsed, 0, _countdownDuration, _rightSegmentLength, 0);
 
+#if NEOPIXEL_DEBUG
+  Serial.print("Showing "); Serial.print(numPixelLeft);
+  Serial.println(" pixels on the left.");
+  Serial.print("Showing "); Serial.print(numPixelRight);
+  Serial.println(" pixels on the right.");
+#endif
+
   int leftSegmentLastIndex = _rightSegmentFirstIndex - 1;
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Setting left segment...");
+#endif
 
   for (int i = 0; i < numPixelLeft; i++) {
     _pixelsPtr->setPixelColor(leftSegmentLastIndex - i, _playerColours[_countdownPlayerLeft - 1]);
+
+#if NEOPIXEL_DEBUG
+    Serial.print(i + 1); Serial.println("th pixel is set.");
+#endif
   }
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Setting right segment...");
+#endif
 
   for (int i = 0; i < numPixelRight; i++) {
     _pixelsPtr->setPixelColor(_rightSegmentFirstIndex + i, _playerColours[_countdownPlayerRight - 1]);
+
+#if NEOPIXEL_DEBUG
+    Serial.print(i + 1); Serial.println("th pixel is set.");
+#endif
   }
 }
 
 void NeopixelCtrl :: countUp(int duration, unsigned long startTime) {
+
+#if NEOPIXEL_DEBUG
+  Serial.println("countUp() is called.");
+#endif
 
   if (_isCountingDown || _isFrenzy) {
     _isCountingDown = false;
@@ -126,26 +212,60 @@ void NeopixelCtrl :: countUp(int duration, unsigned long startTime) {
 
   _countupStartTime = startTime;
   _countupDuration = duration * 1000UL; // convert milliseconds to seconds, UL for typecasting to unsigned long
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Countup start time is "); Serial.println(_countupStartTime);
+  Serial.print("Counting up for "); Serial.print(_countupDuration); Serial.print("ms.");
+#endif
 }
 
 void NeopixelCtrl :: updateCountUp(unsigned long currentTime) {
 
+#if NEOPIXEL_DEBUG
+  Serial.println("updateCountUp() is called.");
+#endif
+
   unsigned long timeElapsed = currentTime - _countupStartTime;
 
+#if NEOPIXEL_DEBUG
+  Serial.print(timeElapsed);
+  Serial.println(" ms has elapsed.");
+#endif
+
   if (timeElapsed > _countupDuration) {
+
+#if NEOPIXEL_DEBUG
+    Serial.println("Duration exceeded. Exiting the function.");
+#endif
+
     _isCountingUp = false;
     return;
   }
 
   int numPixel = map(timeElapsed, 0, _countupDuration, 0, _topSegmentLength);
 
+#if NEOPIXEL_DEBUG
+  Serial.print("Showing "); Serial.print(numPixel);
+  Serial.println(" pixels on each side.");
+#endif
+
   for (int i = 0; i < numPixel; i++) {
     _pixelsPtr->setPixelColor(_topSegmentFirstIndex + i, _pixelsPtr->Color(255, 255, 255));
+
+#if NEOPIXEL_DEBUG
+    Serial.print(i + 1); Serial.println("th pixel is set.");
+#endif
+
   }
 
 }
 
 void NeopixelCtrl :: frenzy(int duration, unsigned long startTime) {
+
+#if NEOPIXEL_DEBUG
+  Serial.println("frenzy() is called.");
+#endif
+
   if (_isCountingDown || _isCountingUp) {
     _isCountingDown = false;
     _isCountingUp = false;
@@ -160,9 +280,16 @@ void NeopixelCtrl :: frenzy(int duration, unsigned long startTime) {
 
 void NeopixelCtrl :: updateFrenzy(unsigned long currentTime) {
 
+#if NEOPIXEL_DEBUG
+  Serial.println("updateFrenzy() is called.");
+#endif
+
   if ((currentTime - _frenzyOldTime > 100UL) && (currentTime - _frenzyStartTime < _frenzyDuration)) {    // for stability, UL for typecasting to unsigned long
     for (int i = 0; i < _topSegmentLength; i++) {
       _pixelsPtr->setPixelColor(_topSegmentFirstIndex + i, _pixelsPtr->Color(random(0, 255), random(0, 255), random(0, 255)));
+#if NEOPIXEL_DEBUG
+      Serial.print(i + 1); Serial.println("th pixel is set.");
+#endif
     }
     _frenzyOldTime = currentTime;
   }
@@ -174,17 +301,34 @@ void NeopixelCtrl :: updateFrenzy(unsigned long currentTime) {
     for (int i = 0; i < _topSegmentLength; i++) {
       _pixelsPtr->setPixelColor(_topSegmentFirstIndex + i, _pixelsPtr->Color(0, 0, 0));
     }
+
+#if NEOPIXEL_DEBUG
+    Serial.println("Duration exceeded. Exiting the function.");
+#endif
   }
 }
 
 void NeopixelCtrl :: displaySpeed(int playerCode, int buttonSpeed) {
   buttonSpeed = constrain(buttonSpeed, 0, MAXSPEED);
   _playerSpeed[playerCode - 1] = buttonSpeed;
-  Serial.println("Adding speed into array");
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Adding Player "); Serial.print(playerCode);
+  Serial.println(" 's speed into the holding array.");
+
+  Serial.print("The speed is ");
+  Serial.println(_playerSpeed[playerCode - 1]);
+#endif
 }
 
 void NeopixelCtrl :: updateSpeed() {
   for (int i = 0; i < _numPlayer; i++) {
+
+#if NEOPIXEL_DEBUG
+    Serial.print("Updating Player "); Serial.print(i + 1);
+    Serial.println("th speed...");
+#endif
+
     int thisPlayerSpeed = _playerSpeed[i];
     int thisPlayerFirstIndex = _playerPixelIndices[i][0];
     int thisPlayerPixelLength = _playerPixelLengths[i];
@@ -192,31 +336,62 @@ void NeopixelCtrl :: updateSpeed() {
 
     int numPixel = map(thisPlayerSpeed, 0, MAXSPEED, 0, thisPlayerPixelLength);
 
+#if NEOPIXEL_DEBUG
+    Serial.print("The speed is ");
+    Serial.println(thisPlayerSpeed);
+    Serial.print("Showing "); Serial.print(numPixel); Serial.println(" pixels.");
+#endif
+
     for (int j = 0; j < numPixel; j++) {
       _pixelsPtr->setPixelColor(thisPlayerFirstIndex + j, thisPlayerColour);
+
+#if NEOPIXEL_DEBUG
+      Serial.print(j + 1); Serial.println("th pixel is set.");
+#endif
+
     }
   }
 }
 
 void NeopixelCtrl :: updatePixelsColors(unsigned long currentTime) {
 
+#if NEOPIXEL_DEBUG
+  Serial.println("updatePixelsColors() is called.");
+#endif
+
   for (int i = 0; i < _totalLength; i++) {
     _pixelsPtr->setPixelColor(i, _pixelsPtr->Color(0, 0, 0));   // "turn off" all neopixels
   }
 
+#if NEOPIXEL_DEBUG
+  Serial.println("Set all pixels to black (0, 0, 0).");
+#endif
+
   updateSpeed();    // always activated so that players can check if the button is working
 
   if (_isCountingDown) {
+#if NEOPIXEL_DEBUG
+    Serial.println("updatePixelsColors: counting down.");
+#endif
     updateCountDown(currentTime);
   }
   else if (_isCountingUp) {
+#if NEOPIXEL_DEBUG
+    Serial.println("updatePixelsColors: counting up.");
+#endif
     updateCountUp(currentTime);
   }
   else if (_isFrenzy) {
+#if NEOPIXEL_DEBUG
+    Serial.println("updatePixelsColors: going frenzy.");
+#endif
     updateFrenzy(currentTime);
   }
 
   _pixelsPtr->show();
+#if NEOPIXEL_DEBUG
+  Serial.println("Showing pixels.");
+#endif
 }
 
 bool NeopixelCtrl :: isCountingDown() {
@@ -233,4 +408,9 @@ bool NeopixelCtrl :: isFrenzy() {
 
 void NeopixelCtrl::setNumPlayer(int numPlayer) {
   _numPlayer = numPlayer;
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Setting the game for "); Serial.print(_numPlayer);
+  Serial.println(" players");
+#endif
 }
--- NeopixelCtrlSplitTop/NeopixelCtrlSplitTopExample/NeopixelCtrlSplitTop.cpp
+++ NeopixelCtrlSplitTop/NeopixelCtrlSplitTopExample/NeopixelCtrlSplitTop.cpp
@@ -5,6 +5,8 @@
   Garage@EEE, School of Electrical and Electronic Engineering, Nanyang Technological University, Singapore.
 */
 
+#define NEOPIXEL_DEBUG true
+
 #include "NeopixelCtrlSplitTop.h"
 
 NeopixelCtrlSplitTop :: NeopixelCtrlSplitTop (Adafruit_NeoPixel* pixelsPtr, int totalLength, int pixelsPin) {
@@ -15,29 +17,61 @@ NeopixelCtrlSplitTop :: NeopixelCtrlSplitTop (Adafruit_NeoPixel* pixelsPtr, int 
   _isCountingDown = false;
   _isCountingUp = false;
   _isFrenzy = false;                 // initialize all actions to false
+
+#if NEOPIXEL_DEBUG
+  Serial.println("The NeopixelCtrl object is created.");
+#endif
 }
 
 void NeopixelCtrlSplitTop :: setLeftSegment(int firstPixel, int lastPixel) {
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Setting the left segment...");
+#endif
+
   _leftSegmentFirstIndex = firstPixel;
   _leftSegmentLastIndex = lastPixel;
 
   _leftSegmentLength = _leftSegmentLastIndex - _leftSegmentFirstIndex + 1;
+#if NEOPIXEL_DEBUG
+  Serial.println("Done.");
+#endif
 }
 
 void NeopixelCtrlSplitTop :: setRightSegment(int firstPixel, int lastPixel) {
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Setting the right segment...");
+#endif
+
   _rightSegmentFirstIndex = firstPixel;
   _rightSegmentLastIndex = lastPixel;
 
   _rightSegmentLength = _rightSegmentLastIndex - _rightSegmentFirstIndex + 1;
+#if NEOPIXEL_DEBUG
+  Serial.println("Done.");
+#endif
 }
 
 void NeopixelCtrlSplitTop :: setPlayerSegment(int playerCode, int firstPixel, int lastPixel) {
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Setting Player "); Serial.print(playerCode); Serial.println("'s segment...");
+#endif
+
   _playerPixelIndices[playerCode - 1][0] = firstPixel;
   _playerPixelIndices[playerCode - 1][1] = lastPixel;
   _playerPixelLengths[playerCode - 1] = lastPixel - firstPixel + 1;
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Done.");
+#endif
 }
 
 uint32_t NeopixelCtrlSplitTop :: getColour(char colour) {
+#if NEOPIXEL_DEBUG
+  Serial.print("Getting colour...");
+#endif
   switch (colour) {
     case 'R':
       return _pixelsPtr->Color(255, 0, 0);
@@ -54,14 +88,22 @@ uint32_t NeopixelCtrlSplitTop :: getColour(char colour) {
     default:
       return _pixelsPtr->Color(255, 255, 255);                      //return white if the colour input is undefined
   }
+#if NEOPIXEL_DEBUG
+  Serial.println("Done.");
+#endif
 }
 
 void NeopixelCtrlSplitTop :: setPlayerSegmentColour(int playerCode, char colour) {
+#if NEOPIXEL_DEBUG
+  Serial.print("Setting colour...");
+#endif
   _playerColours[playerCode - 1] = getColour(colour);
 }
 
 void NeopixelCtrlSplitTop :: countDown(int playerCode1, int playerCode2, int duration, unsigned long startTime) {
-
+#if NEOPIXEL_DEBUG
+  Serial.println("countDown() is called.");
+#endif
   if (_isCountingUp || _isFrenzy) {
     _isCountingUp = false;
     _isFrenzy = false;
@@ -78,15 +120,39 @@ void NeopixelCtrlSplitTop :: countDown(int playerCode1, int playerCode2, int dur
   _countdownPlayerLeft = playerCode1;
   _countdownPlayerRight = playerCode2;
 
+#if NEOPIXEL_DEBUG
+  Serial.print("Left player is Player"); Serial.println(_countdownPlayerLeft);
+  Serial.print("Right player is Player"); Serial.println(_countdownPlayerRight);
+#endif
+
   _countdownStartTime = startTime;
   _countdownDuration = duration * 1000UL; // convert seconds to milliseconds, UL for typecasting to unsigned long
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Countdown start time is "); Serial.println(_countdownStartTime);
+  Serial.print("Counting down for "); Serial.print(_countdownDuration); Serial.print("ms.");
+#endif
 }
 
 void NeopixelCtrlSplitTop :: updateCountDown(unsigned long currentTime) {
 
+#if NEOPIXEL_DEBUG
+  Serial.println("updateCountDown() is called.");
+#endif
+
   unsigned long timeElapsed = currentTime - _countdownStartTime;
 
+#if NEOPIXEL_DEBUG
+  Serial.print(timeElapsed);
+  Serial.println(" ms has elapsed.");
+#endif
+
   if (timeElapsed > _countdownDuration) {
+
+#if NEOPIXEL_DEBUG
+    Serial.println("Duration exceeded. Exiting the function.");
+#endif
+
     _isCountingDown = false;
     return;
   }
@@ -94,16 +160,47 @@ void NeopixelCtrlSplitTop :: updateCountDown(unsigned long currentTime) {
   int numPixelLeft = map(timeElapsed, 0, _countdownDuration, _leftSegmentLength, 0);
   int numPixelRight = map(timeElapsed, 0, _countdownDuration, _rightSegmentLength, 0);  // mapping from high to low (counting down)
 
+#if NEOPIXEL_DEBUG
+  Serial.print("Showing "); Serial.print(numPixelLeft);
+  Serial.println(" pixels on the left.");
+  Serial.print("Showing "); Serial.print(numPixelRight);
+  Serial.println(" pixels on the right.");
+#endif
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Setting left segment...");
+#endif
   for (int i = 0; i < numPixelLeft; i++) {
     _pixelsPtr->setPixelColor(_leftSegmentLastIndex - i, _playerColours[_countdownPlayerLeft - 1]);  // displaying from center to edge
+
+#if NEOPIXEL_DEBUG
+    Serial.print(i + 1); Serial.println("th pixel is set.");
+#endif
+
   }
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Setting right segment...");
+#endif
 
   for (int i = 0; i < numPixelRight; i++) {
     _pixelsPtr->setPixelColor(_rightSegmentFirstIndex + i, _playerColours[_countdownPlayerRight - 1]); // displaying from center to edge
+
+
+#if NEOPIXEL_DEBUG
+    Serial.print(i + 1); Serial.println("th pixel is set.");
+#endif
+
+
   }
 }
 
 void NeopixelCtrlSplitTop :: countUp(int duration, unsigned long startTime) {
+
+#if NEOPIXEL_DEBUG
+  Serial.println("countUp() is called.");
+#endif
+
 
   if (_isCountingDown || _isFrenzy) {
     _isCountingDown = false;
@@ -114,13 +211,32 @@ void NeopixelCtrlSplitTop :: countUp(int duration, unsigned long startTime) {
 
   _countupStartTime = startTime;
   _countupDuration = duration * 1000UL; // convert seconds to milliseconds, UL for typecasting to unsigned long
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Countup start time is "); Serial.println(_countupStartTime);
+  Serial.print("Counting up for "); Serial.print(_countupDuration); Serial.print("ms.");
+#endif
 }
 
 void NeopixelCtrlSplitTop :: updateCountUp(unsigned long currentTime) {
 
+#if NEOPIXEL_DEBUG
+  Serial.println("updateCountUp() is called.");
+#endif
+
   unsigned long timeElapsed = currentTime - _countupStartTime;
 
+#if NEOPIXEL_DEBUG
+  Serial.print(timeElapsed);
+  Serial.println(" ms has elapsed.");
+#endif
+
   if (timeElapsed > _countupDuration) {
+
+#if NEOPIXEL_DEBUG
+    Serial.println("Duration exceeded. Exiting the function.");
+#endif
+
     _isCountingUp = false;
     return;
   }
@@ -128,17 +244,47 @@ void NeopixelCtrlSplitTop :: updateCountUp(unsigned long currentTime) {
   int numPixelLeft = map(timeElapsed, 0, _countdownDuration, 0, _leftSegmentLength);
   int numPixelRight = map(timeElapsed, 0, _countdownDuration, 0, _rightSegmentLength);
 
+#if NEOPIXEL_DEBUG
+  Serial.print("Showing "); Serial.print(numPixelLeft);
+  Serial.println(" pixels on the left.");
+  Serial.print("Showing "); Serial.print(numPixelRight);
+  Serial.println(" pixels on the right.");
+#endif
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Setting left segment...");
+#endif
+
   for (int i = 0; i < numPixelLeft; i++) {
     _pixelsPtr->setPixelColor(_leftSegmentLastIndex - i, _playerColours[_countdownPlayerLeft - 1]);
+
+#if NEOPIXEL_DEBUG
+    Serial.print(i + 1); Serial.println("th pixel is set.");
+#endif
+
   }
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Setting right segment...");
+#endif
 
   for (int i = 0; i < numPixelRight; i++) {
     _pixelsPtr->setPixelColor(_rightSegmentFirstIndex + i, _playerColours[_countdownPlayerRight - 1]);
+
+#if NEOPIXEL_DEBUG
+    Serial.print(i + 1); Serial.println("th pixel is set.");
+#endif
+
   }
 
 }
 
 void NeopixelCtrlSplitTop :: frenzy(int duration, unsigned long startTime) {
+
+#if NEOPIXEL_DEBUG
+  Serial.println("frenzy() is called.");
+#endif
+
   if (_isCountingDown || _isCountingUp) {
     _isCountingDown = false;
     _isCountingUp = false;
@@ -154,12 +300,22 @@ void NeopixelCtrlSplitTop :: frenzy(int duration, unsigned long startTime) {
 
 void NeopixelCtrlSplitTop :: updateFrenzy(unsigned long currentTime) {
 
+#if NEOPIXEL_DEBUG
+  Serial.println("updateFrenzy() is called.");
+#endif
+
   if ((currentTime - _frenzyOldTime > 100UL) && (currentTime - _frenzyStartTime < _frenzyDuration)) {    // for stability, UL for typecasting to unsigned long
     for (int i = 0; i < _leftSegmentLength; i++) {
       _pixelsPtr->setPixelColor(_leftSegmentFirstIndex + i, _pixelsPtr->Color(random(0, 255), random(0, 255), random(0, 255)));
+#if NEOPIXEL_DEBUG
+      Serial.print(i + 1); Serial.println("th pixel on the left is set.");
+#endif
     }
     for (int i = 0; i < _rightSegmentLength; i++) {
       _pixelsPtr->setPixelColor(_rightSegmentFirstIndex + i, _pixelsPtr->Color(random(0, 255), random(0, 255), random(0, 255)));
+#if NEOPIXEL_DEBUG
+      Serial.print(i + 1); Serial.println("th pixel on the right is set.");
+#endif
     }
     _frenzyOldTime = currentTime;
   }
@@ -174,12 +330,97 @@ void NeopixelCtrlSplitTop :: updateFrenzy(unsigned long currentTime) {
     for (int i = 0; i < _rightSegmentLength; i++) {
       _pixelsPtr->setPixelColor(_rightSegmentFirstIndex + i, _pixelsPtr->Color(0, 0, 0));
     }
+
+#if NEOPIXEL_DEBUG
+    Serial.println("Duration exceeded. Exiting the function.");
+#endif
   }
 }
 
 void NeopixelCtrlSplitTop :: displaySpeed(int playerCode, int buttonSpeed) {
   buttonSpeed = constrain(buttonSpeed, 0, MAXSPEED);
   _playerSpeed[playerCode - 1] = buttonSpeed;
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Adding Player "); Serial.print(playerCode);
+  Serial.println(" 's speed into the holding array.");
+
+  Serial.print("The speed is ");
+  Serial.println(_playerSpeed[playerCode - 1]);
+#endif
+}
+
+void NeopixelCtrlSplitTop :: updateSpeed() {
+  for (int i = 0; i < _numPlayer; i++) {
+
+#if NEOPIXEL_DEBUG
+    Serial.print("Updating Player "); Serial.print(i + 1);
+    Serial.println("th speed...");
+#endif
+
+    int thisPlayerSpeed = _playerSpeed[i];
+    int thisPlayerFirstIndex = _playerPixelIndices[i][0];
+    int thisPlayerPixelLength = _playerPixelLengths[i];
+    uint32_t thisPlayerColour = _playerColours[i];
+
+    int numPixel = map(thisPlayerSpeed, 0, MAXSPEED, 0, thisPlayerPixelLength);
+
+#if NEOPIXEL_DEBUG
+    Serial.print("The speed is ");
+    Serial.println(thisPlayerSpeed);
+    Serial.print("Showing "); Serial.print(numPixel); Serial.println(" pixels.");
+#endif
+
+    for (int j = 0; j < numPixel; j++) {
+      _pixelsPtr->setPixelColor(thisPlayerFirstIndex + j, thisPlayerColour);
+
+#if NEOPIXEL_DEBUG
+      Serial.print(j + 1); Serial.println("th pixel is set.");
+#endif
+
+    }
+  }
+}
+
+void NeopixelCtrlSplitTop :: updatePixelsColors(unsigned long currentTime) {
+
+#if NEOPIXEL_DEBUG
+  Serial.println("updatePixelsColors() is called.");
+#endif
+
+  for (int i = 0; i < _totalLength; i++) {
+    _pixelsPtr->setPixelColor(i, _pixelsPtr->Color(0, 0, 0));   // "turn off" all neopixels
+  }
+
+#if NEOPIXEL_DEBUG
+  Serial.println("Set all pixels to black (0, 0, 0).");
+#endif
+
+  updateSpeed();    // always activated so that players can check if the button is working
+
+  if (_isCountingDown) {
+#if NEOPIXEL_DEBUG
+    Serial.println("updatePixelsColors: counting down.");
+#endif
+    updateCountDown(currentTime);
+  }
+  else if (_isCountingUp) {
+#if NEOPIXEL_DEBUG
+    Serial.println("updatePixelsColors: counting up.");
+#endif
+    updateCountUp(currentTime);
+  }
+  else if (_isFrenzy) {
+#if NEOPIXEL_DEBUG
+    Serial.println("updatePixelsColors: going frenzy.");
+#endif
+    updateFrenzy(currentTime);
+  }
+
+  _pixelsPtr->show();
+#if NEOPIXEL_DEBUG
+  Serial.println("Showing pixels.");
+#endif
 }
 
 bool NeopixelCtrlSplitTop :: isCountingDown() {
@@ -194,42 +435,11 @@ bool NeopixelCtrlSplitTop :: isFrenzy() {
   return _isFrenzy;
 }
 
-void NeopixelCtrlSplitTop :: updateSpeed() {
-  for (int i = 0; i < _numPlayer; i++) {
-    int thisPlayerSpeed = _playerSpeed[i];
-    int thisPlayerFirstIndex = _playerPixelIndices[i][0];
-    int thisPlayerPixelLength = _playerPixelLengths[i];
-    uint32_t thisPlayerColour = _playerColours[i];
-
-    int numPixel = map(thisPlayerSpeed, 0, MAXSPEED, 0, thisPlayerPixelLength);
-
-    for (int j = 0; j < numPixel; j++) {
-      _pixelsPtr->setPixelColor(thisPlayerFirstIndex + j, thisPlayerColour);
-    }
-  }
-}
-
-void NeopixelCtrlSplitTop :: updatePixelsColors(unsigned long currentTime) {
-
-  for (int i = 0; i < _totalLength; i++) {
-    _pixelsPtr->setPixelColor(i, _pixelsPtr->Color(0, 0, 0));   // "turn off" all neopixels
-  }
-
-  updateSpeed();    // always activated so that players can check if the button is working
-
-  if (_isCountingDown) {
-    updateCountDown(currentTime);
-  }
-  else if (_isCountingUp) {
-    updateCountUp(currentTime);
-  }
-  else if (_isFrenzy) {
-    updateFrenzy(currentTime);
-  }
-
-  _pixelsPtr->show();
-}
-
 void NeopixelCtrlSplitTop :: setNumPlayer(int numPlayer) {
   _numPlayer = numPlayer;
+
+#if NEOPIXEL_DEBUG
+  Serial.print("Setting the game for "); Serial.print(_numPlayer);
+  Serial.println(" players");
+#endif
 }
